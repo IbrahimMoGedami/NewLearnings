70e8768822f662a6eefeba222d1deb5302633c60
Ibrahim Gedami <ibrahim.abdelgani@seddiqiholding.com>
2025-04-23 14:39:31 +0400

    Initial Commit

---
+++ NewLearnings/NewLearningsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewLearningsApp.swift
+//  NewLearnings
+//
+//  Created by Ibrahim Mo Gedami on 4/23/25.
+//
+
+import SwiftUI
+
+@main
+struct NewLearningsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
